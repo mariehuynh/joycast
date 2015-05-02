fde578a389d622b21f6e8c5900fe97adda45416d
Nickolas Fotopoulos <nickolas.fotopoulos@synaptics.com>
2015-05-02 13:30:15 -0700

    Added code snippets showing how to get Emotiv EPOC data and how to connect to Twitter

---
+++ reference_code_snippets/tweepy_demo.py
@@ -0,0 +1,22 @@
+"""
+Cobbled together, mostly from
+http://www.dototot.com/reply-tweets-python-tweepy-twitter-bot/
+"""
+import tweepy
+
+# You must create your own keys.py with a keys dict in it. Do not commit it!
+from keys import keys
+
+SCREEN_NAME = keys['screen_name']
+CONSUMER_KEY = keys['consumer_key']
+CONSUMER_SECRET = keys['consumer_secret']
+ACCESS_TOKEN = keys['access_token']
+ACCESS_TOKEN_SECRET = keys['access_token_secret']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print tweet.text
