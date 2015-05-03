339bac491566869723b3b42d4aecc8f5cbdae000
Nickolas Fotopoulos <nickolas.fotopoulos@synaptics.com>
2015-05-03 12:51:35 -0700

    emo: make profile loading work better

--- joycast/emo.py
+++ joycast/emo.py
@@ -137,6 +137,7 @@ EE_LoadUserProfile = libEDK.EE_LoadUserProfile
 EE_LoadUserProfile.restype = ctypes.c_int
 EE_LoadUserProfile.argtypes = (ctypes.c_uint, ctypes.c_char_p)
 
+EE_UserAdded = 16
 EE_EmoStateUpdated = 64  # libEDK.EE_Event_enum.EE_EmoStateUpdated
 EDK_OK = 0
 
@@ -177,15 +178,24 @@ def getNextHappiness(connectionType, profileFile=None):
     else:
         raise ValueError("option must be 1 (headset) or 2 (EmoComposer)")
 
-    if profileFile is not None:
-        status = EE_LoadUserProfile(ctypes.c_uint(0), abspath(profileFile))
-        if status != EDK_OK:
-            raise IOError("could not load user profile: path {0:s}, status {1:d}".format(abspath(profileFile), status))
-
-    userID = ctypes.c_uint(9999)
-    eEvent = EE_EmoEngineEventCreate()
-    eState = EE_EmoStateCreate()
     try:
+        userID = ctypes.c_uint(9999)
+        eEvent = EE_EmoEngineEventCreate()
+        eState = EE_EmoStateCreate()
+
+        if profileFile is not None:
+            time.sleep(0.1)
+            state = EE_EngineGetNextEvent(eEvent)
+            if state == EDK_OK:
+                eventType = EE_EmoEngineEventGetType(eEvent)
+                if eventType == EE_UserAdded:
+                    EE_EmoEngineEventGetUserId(eEvent, ctypes.pointer(userID))
+                    status = EE_LoadUserProfile(userID, abspath(profileFile))
+                    if status != EDK_OK:
+                        raise IOError("could not load user profile: path {0:s}, status {1:d}".format(abspath(profileFile), status))
+            else:
+                raise IOError("could not load user profile: path {0:s}, status {1:d}".format(abspath(profileFile), state))
+
         while 1:
             time.sleep(0.1)
             state = EE_EngineGetNextEvent(eEvent)
