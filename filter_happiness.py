42493dfa5704037c4cf209c111d1e22a975e37ea
Nickolas Fotopoulos <nickolas.fotopoulos@synaptics.com>
2015-05-02 18:36:16 -0700

    Add filter_happiness.py for experimenting with different filters

---
+++ filter_happiness.py
@@ -0,0 +1,20 @@
+from sys import argv
+
+HISTORY_LEN = 20
+class HappinessFilter(object):
+    def __init__(self):
+        self.history = [0] * HISTORY_LEN
+
+    def update(self, x):
+        self.history = self.history[1:] + [x]
+        mean = sum(self.history) / HISTORY_LEN
+        return mean
+
+logFilename = argv[1]
+
+myFilter = HappinessFilter()
+with open(logFilename) as f:
+    for line in f:
+        happiness = float(line.rstrip("\n"))
+        filteredHappiness = myFilter.update(happiness)
+        print(filteredHappiness)
